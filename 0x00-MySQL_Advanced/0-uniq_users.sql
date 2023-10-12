a8736d89629ed43f8853a7f338a4309692917eb6
Samuel <a2jsamuel@gmail.com>
2023-10-12 11:40:31 +0300

    Add unique to email in 0-file

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
-    email varchar(255),
+    email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
     PRIMARY KEY (id)
 );

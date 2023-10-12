2773d24c18bd620e36dcaa111497d8fd017dfd50
Samuel <a2jsamuel@gmail.com>
2023-10-12 12:58:41 +0300

    Remove orderby in 3-file

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
     FROM metal_bands
-    GROUP BY band_name
     WHERE style LIKE 'Glam rock%'
     ORDER BY lifespan DESC;

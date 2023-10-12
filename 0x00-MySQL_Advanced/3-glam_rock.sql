ca48b3bee5d0334d3f9702ad9edc351ea1f4ff91
Samuel <a2jsamuel@gmail.com>
2023-10-12 12:34:59 +0300

    fix and add if null to split to yr 2022

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (split - formed) AS lifespan
+SELECT band_name, (IF NULL(split, 2022) - formed) AS lifespan
     FROM metal_bands
     GROUP BY band_name
     WHERE style='Glam rock'

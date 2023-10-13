7a70041dd72dfba46a3db6f6f15adda38478e611
Samuel <a2jsamuel@gmail.com>
2023-10-13 14:13:35 +0300

    This is to fix index to 1st index of column!

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,2 @@
 -- SQL script that creates an index idx_name_first on the table names and the first letter of name
-CREATE INDEX name_fri ON names(name FROM 1 TO 1);
+CREATE INDEX name_fri ON names(name(1));

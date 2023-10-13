3c62ef3321631fa421a388d8fd331c678571e81c
Samuel <a2jsamuel@gmail.com>
2023-10-13 21:42:58 +0300

    Fix and mod div to divs in 10-file

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,12 +1,13 @@
 -- SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0
 DELIMITER $
 CREATE FUNCTION SafeDiv (a INT, b INT)
-RETURNS FLOAT
+RETURNS FLOAT deterministic
 BEGIN
-    declare div float default 0;
+    declare divs FLOAT default 0;
     IF b != 0 THEN
-       SET div = a / b;
+       SET divs = a / b;
+       RETURN divs;
     END IF;
-    RETURN div;
+    RETURN divs;
 END $
 DELIMITER ;

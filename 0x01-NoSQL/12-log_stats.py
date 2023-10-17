16ae90cee69542307cea4425f60f67955c3108fb
Samuel <a2jsamuel@gmail.com>
2023-10-17 23:03:11 +0300

    see and fix for 12-file

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 """
-script that provides some stats about Nginx logs stored in MongoDB
+Module that has a Python function which provides some stats about Nginx logs
 """
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs = client.logs.nginx
-    # get number of documents in collection
-    docs_num = nginx_logs.count_documents({})
-    get_num = nginx_logs.count_documents({'method': 'GET'})
-    post_num = nginx_logs.count_documents({'method': 'POST'})
-    put_num = nginx_logs.count_documents({'method': 'PUT'})
-    patch_num = nginx_logs.count_documents({'method': 'PATCH'})
-    delete_num = nginx_logs.count_documents({'method': 'DELETE'})
-    get_status = nginx_logs.count_documents({'method': 'GET',
-                                             'path': '/status'})
-    print("{} logs".format(docs_num))
+
+    logs_stats = client.logs.nginx
+    
+    cou_doc = logs_stats.count_documents({})
+    get_doc = logs_stats.count_documents( { "method": "GET" } )
+    post_doc = logs_stats.count_documents( {"method": "POST"} )
+    put_doc = logs_stats.count_documents( { "method": "PUT" } )
+    patch_doc = logs_stats.count_documents( { "method": "PATCH" } )
+    del_doc = logs_stats.count_documents( { "method": "DELETE" } )
+    status_doc = logs_stats.count_documents( { "method": "GET",
+                                              "path": "/status" } )
+
+    print('{} logs'.format(cou_doc))
     print("Methods:")
-    print("\tmethod GET: {}".format(get_num))
-    print("\tmethod POST: {}".format(post_num))
-    print("\tmethod PUT: {}".format(put_num))
-    print("\tmethod PATCH: {}".format(patch_num))
-    print("\tmethod DELETE: {}".format(delete_num))
-    print("{} status check".format(get_status))
+    print('    method GET: {}'.format(get_doc))
+    print('    method POST: {}'.format(post_doc))
+    print('    method PUT: {}'.format(put_doc))
+    print('    method PATCH: {}'.format(patch_doc))
+    print('    method DELETE: {}'.format(del_doc))
+    print('{} status check'.format(status_doc))

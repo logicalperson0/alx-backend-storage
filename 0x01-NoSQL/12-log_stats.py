2d69211ea82124b902960753bbb8a85c8bedbf6a
Samuel <a2jsamuel@gmail.com>
2023-10-17 22:58:33 +0300

    fix for 12-file!

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -11,18 +11,19 @@ if __name__ == "__main__":
     logs_stats = client.logs.nginx
     
     cou_doc = logs_stats.count_documents({})
-    get_doc = logs_stats.count_documents({ "method": "GET" })
+    get_doc = logs_stats.count_documents( { "method": "GET" } )
     post_doc = logs_stats.count_documents( {"method": "POST"} )
     put_doc = logs_stats.count_documents( { "method": "PUT" } )
     patch_doc = logs_stats.count_documents( { "method": "PATCH" } )
     del_doc = logs_stats.count_documents( { "method": "DELETE" } )
-    status_doc = logs_stats.count_documents( { "method": "GET", "path": "/status" } )
+    status_doc = logs_stats.count_documents( { "method": "GET",
+                                              "path": "/status" } )
 
     print('{} logs'.format(cou_doc))
     print("Methods:")
-    print('\tmethod GET: {}'.format(get_doc))
-    print('\tmethod POST: {}'.format(post_doc))
-    print('\tmethod PUT: {}'.format(put_doc))
-    print('\tmethod PATCH: {}'.format(patch_doc))
-    print('\tmethod DELETE: {}'.format(del_doc))
+    print('    method GET: {}'.format(get_doc))
+    print('    method POST: {}'.format(post_doc))
+    print('    method PUT: {}'.format(put_doc))
+    print('    method PATCH: {}'.format(patch_doc))
+    print('    method DELETE: {}'.format(del_doc))
     print('{} status check'.format(status_doc))

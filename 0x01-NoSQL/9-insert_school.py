7438f177ce4e8202fa632ed32a845e556ebed5dc
Samuel <a2jsamuel@gmail.com>
2023-10-17 19:02:45 +0300

    This is for task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module that has a Python function which lists all documents in a collection
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """function that inserts a new document in a collection based
+    on kwargs"""
+    if mongo_collection is None:
+        return []
+    create_id = mongo_collection.insert_one(kwargs)
+    return create_id

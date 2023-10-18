abeb1269ad2efffdfe9b0f8b9a38cc32eb3dfd7d
Samuel <a2jsamuel@gmail.com>
2023-10-18 21:57:12 +0300

    This is for task 0 on exercise file

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+contians a Cache class defn for redis
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Redis client as a private variable named _redis (using redis.Redis())
+    and flush the instance using flushdb"""
+    def __init__(self) -> None:
+        """init a Redis client"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """method that takes a data argument and returns a string"""
+        ran_key = uuid.uuid4()
+        self._redis.set(ran_key, data)
+        return str(ran_key)
